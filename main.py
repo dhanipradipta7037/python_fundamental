a53e23489213852d02d4a17990a7f2727964d765
gededhani <dhanipradipta777@gmail.com>
2023-05-28 14:08:31 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu"')
+print("Maka Budi berangkat ke toko")
+print("dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 175
+
+if jumlah_botol_susu > 0:
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyerahkan hasilnya kepada ibu")
+
