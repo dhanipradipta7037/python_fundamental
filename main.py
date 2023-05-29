7189a6c85b321c5b4878a83ac3f9808cad44b4e4
gededhani <dhanipradipta777@gmail.com>
2023-05-29 13:54:51 +0800

    initial commit

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ print("dan mulai berbelanja")
 
 # Percabangan
 jumlah_botol_susu = 175
+jumlah_telur = 1500
+
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"Jumlah telur {jumlah_telur} telur")
 
 if jumlah_botol_susu > 0:
-    print("Budi membeli 1 botol susu")
+    print("Budi mengecek harganya dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
 
