f9f732bb106097d5b0c120b3ebdbe436883196a2
gededhani <dhanipradipta777@gmail.com>
2023-05-29 15:13:48 +0800

    initial commit

---
+++ python_Perulangan.py
@@ -0,0 +1,20 @@
+# Perluangan
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua bukumu"')
+
+jml_buku_yg_dibaca = 0
+print(f'Jumlah buku yang sudah dibaca {jml_buku_yg_dibaca}')
+
+for jml_buku_yg_dibaca in range(1,11):
+    print(f'Buku ke {jml_buku_yg_dibaca} sudah dibaca')
+
+print(f'Jumlah buku yang sudah dibaca {jml_buku_yg_dibaca}')
+
+
+# Tanpa Perulangan
+print("TANPA FOR")
+print('Membaca buku ke-1')
+print('Membaca buku ke-2')
+print('Membaca buku ke-3')
+print('Membaca buku ke-4')
+print('Membaca buku ke-5')
